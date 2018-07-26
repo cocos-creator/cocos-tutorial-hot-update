e819361152502b41078b27dbd87712d71cd12e46
pandamicro <huabin.ling@cocos2d-x.org>
2018-07-26 16:13:59 +0800

    Add default template main.js

---
+++ build-templates/jsb-default/main.js
@@ -0,0 +1,114 @@
+window.boot = function () {
+    var settings = window._CCSettings;
+    window._CCSettings = undefined;
+
+    if ( !settings.debug ) {
+        var uuids = settings.uuids;
+
+        var rawAssets = settings.rawAssets;
+        var assetTypes = settings.assetTypes;
+        var realRawAssets = settings.rawAssets = {};
+        for (var mount in rawAssets) {
+            var entries = rawAssets[mount];
+            var realEntries = realRawAssets[mount] = {};
+            for (var id in entries) {
+                var entry = entries[id];
+                var type = entry[1];
+                // retrieve minified raw asset
+                if (typeof type === 'number') {
+                    entry[1] = assetTypes[type];
+                }
+                // retrieve uuid
+                realEntries[uuids[id] || id] = entry;
+            }
+        }
+
+        var scenes = settings.scenes;
+        for (var i = 0; i < scenes.length; ++i) {
+            var scene = scenes[i];
+            if (typeof scene.uuid === 'number') {
+                scene.uuid = uuids[scene.uuid];
+            }
+        }
+
+        var packedAssets = settings.packedAssets;
+        for (var packId in packedAssets) {
+            var packedIds = packedAssets[packId];
+            for (var j = 0; j < packedIds.length; ++j) {
+                if (typeof packedIds[j] === 'number') {
+                    packedIds[j] = uuids[packedIds[j]];
+                }
+            }
+        }
+    }
+
+    var onStart = function () {
+        cc.loader.downloader._subpackages = settings.subpackages;
+
+        cc.view.enableRetina(true);
+        cc.view.resizeWithBrowserSize(true);
+
+        // init assets
+        cc.AssetLibrary.init({
+            libraryPath: 'res/import',
+            rawAssetsBase: 'res/raw-',
+            rawAssets: settings.rawAssets,
+            packedAssets: settings.packedAssets,
+            md5AssetsMap: settings.md5AssetsMap
+        });
+
+        var launchScene = settings.launchScene;
+
+        // load scene
+        cc.director.loadScene(launchScene, null,
+            function () {
+                cc.loader.onProgress = null;
+                console.log('Success to load scene: ' + launchScene);
+            }
+        );
+    };
+
+    // jsList
+    var jsList = settings.jsList;
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.js';
+    if (jsList) {
+        jsList = jsList.map(function (x) {
+            return 'src/' + x;
+        });
+        jsList.push(bundledScript);
+    }
+    else {
+        jsList = [bundledScript];
+    }
+
+    // anysdk scripts
+    if (cc.sys.isNative && cc.sys.isMobile) {
+        jsList = jsList.concat(['src/anysdk/jsb_anysdk.js', 'src/anysdk/jsb_anysdk_constants.js']);
+    }
+
+    var option = {
+        id: 'GameCanvas',
+        scenes: settings.scenes,
+        debugMode: settings.debug ? cc.debug.DebugMode.INFO : cc.debug.DebugMode.ERROR,
+        showFPS: settings.debug,
+        frameRate: 60,
+        jsList: jsList,
+        groupList: settings.groupList,
+        collisionMatrix: settings.collisionMatrix,
+        renderMode: 0
+    }
+
+    cc.game.run(option, onStart);
+};
+
+if (jsb) {
+    var hotUpdateSearchPaths = localStorage.getItem('HotUpdateSearchPaths');
+    if (hotUpdateSearchPaths) {
+        jsb.fileUtils.setSearchPaths(JSON.parse(hotUpdateSearchPaths)); 
+    }
+}
+
+require('src/settings.js');
+require('src/cocos2d-jsb.js');
+require('jsb-adapter/engine/index.js');
+window.boot();
